5020d406ebf4f037b24b942c66848e94607576ad
SwingZ Admin <mike.swinger@gmx.de>
2026-05-02 12:51:52 +0200

    Fix Vercel deployment: remove output: 'standalone' from next.config.js

--- next.config.js
+++ next.config.js
@@ -12,7 +12,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'standalone',
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
